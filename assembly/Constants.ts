8fc71c1b331a2c7c00077f85fc130e2ef09c41b0
jpw <user@user.com>
2024-05-09 14:43:11 -0400

    initial commit

---
+++ assembly/Constants.ts
@@ -0,0 +1,17 @@
+import { Base58 } from "@koinos/sdk-as";
+
+export namespace Constants {
+  export const NAME: string = "KoinosAgents";
+  export const SYMBOL: string = "AGNT";
+  export const MINT_PRICE: u64 = 9553500000;
+  //enable again for mint fee
+  //export const MINT_FEE: bool = true;
+  export const MINT_FEE: bool = false;
+  export const MAX_SUPPLY: u64 = 10000;
+  export const URI: string = "https://gateway.pinata.cloud/ipfs/QmQ7HBPQvWWJ5iZrtGkgRSnRQWDbnVS56HXsivH5hKcuNz";
+  export const OWNER: Uint8Array = Base58.decode("1E13mrsSJW4LAHCHavS8CNurTM1Z9367dX");
+
+  // token mint
+  export const TOKEN_PAY: Uint8Array = Base58.decode("15DJN4a8SgrbGhhGksSBASiSYjGnMU8dGL");
+  export const ADDRESS_PAY: Uint8Array = Base58.decode("1F9gLGQvyjz7X3JTWnWPxapkMgekyxREW7");
+}
